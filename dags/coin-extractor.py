46134468917c968d7ae5ce7237bf7f071bbbce3c
rodrigo.haddad <rgh@poli.ufrj.br>
2021-08-14 17:28:34 -0300

    added pipfile

--- dags/coin-extractor.py
+++ dags/coin-extractor.py
@@ -1,26 +1,34 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime
-#from scripts.get_coins_price import get_coins_price
+import os
 
 with DAG("coin-extractor",
          start_date=datetime(2021, 8, 13),
          schedule_interval="0 7 * * *") as dag:
+    day = datetime.now()
+    env = os.environ.copy()
+    env.update({
+        'GOOGLE_APPLICATION_CREDENTIALS': '/opt/airflow/dags/credentials.json',
+        'ds': day.strftime("%Y-%m-%d"),
+        'ds_no_dash': day.strftime("%Y%m%d"),
+        'ds_coin': day.strftime("%d-%m-%Y"),
+    })
+
     start = DummyOperator(task_id="start")
     end = DummyOperator(task_id="end")
 
     get_coins = BashOperator(
         task_id="get_coins",
         bash_command=f'python3 /opt/airflow/dags/scripts/get_coins_price.py',
-        #env=env,
+        env=env,
     )
 
     # get_coins = PythonOperator(task_id="get_coins",
     #                            python_callable=get_coins_price)
 
-    # get_coins_validator = PythonOperator(task_id="get_coins_validator",
+    # get_coins_validator = PythonOperator(task_id="val_get_coins",
     #                                      python_callable=get_coins_price)
 
     # get_coins >> get_coins_validator
--- dags/scripts/get_coins_price.py
+++ dags/scripts/get_coins_price.py
@@ -2,13 +2,22 @@ from google.cloud import bigquery
 import requests
 import pandas as pd
 from datetime import datetime
+import os
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+local_vars = locals()
 
 
 class CoinsPrices:
     def __init__(self):
+        config = {**local_vars}
+        config.update({k.lower(): v for k, v in os.environ.items()})
+
         now = datetime.today()
-        self.partition_date = now.strftime("%Y-%m-%d")
-        self.partition_date_no_dash = now.strftime("%Y%m%d")
+        self.partition_date = config.get('ds', now.strftime("%Y-%m-%d"))
+        self.partition_date_no_dash = config.get('ds_no_dash', now.strftime("%Y%m%d"))
+        self.date_coin = config.get('ds_coin', now.strftime("%d-%m-%Y"))
 
         self.coins_ids = ["bitcoin", "ethereum"]
         self.info_fields = ["id", "symbol", "name"]
@@ -17,11 +26,27 @@ class CoinsPrices:
 
         self.client_bq = self.create_bq_client()
 
-    def get_coins_price(self, coin_id: str):
+        self.http_adapter = self.get_adapter()
+
+    @staticmethod
+    def get_adapter():
+        retry_strategy = Retry(
+            total=3,
+            backoff_factor=30
+        )
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+        http = requests.Session()
+        http.mount("https://", adapter)
+        return http
+
+    def get_coins_price(self, coin_id: str) -> pd.DataFrame:
         endpoint = f"coins/{coin_id}/history"
-        http_response = requests.get(url=f"https://api.coingecko.com/api/v3/{endpoint}",
-                                     params={"date": self.partition_date, "location": False})
-        coin_info = http_response.json()
+        try:
+            http_response = self.http_adapter.get(url=f"https://api.coingecko.com/api/v3/{endpoint}",
+                                                  params={"date": self.date_coin, "location": False})
+            coin_info = http_response.json()
+        except:
+            return self.empty_row_for_error()
 
         row = [
             {
@@ -36,6 +61,19 @@ class CoinsPrices:
         ]
 
         return pd.DataFrame(row)
+
+    def empty_row_for_error(self) -> pd.DataFrame:
+        return pd.DataFrame([
+            {
+                "id": "",
+                "symbol": "",
+                "name": "",
+                "snapshot_date": datetime.strptime(self.partition_date, '%Y-%m-%d').date(),
+                "current_price_usd": 0.0,
+                "current_price_eur": 0.0,
+                "current_price_brl": 0.0,
+            }
+        ])
 
     def create_bq_client(self):
         return bigquery.Client(
